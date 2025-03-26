21498692cd14182779223832dcd21b25524175db
Aryan Sharma <aryansharma2k4@gmail.com>
2025-03-26 20:39:45 +0530

    Revert "Merge pull request #2 from AdwaitMishr/main"
    This reverts commit 5f5a0dd6c43350998a28f6d3add8d4ca54f32f6e, reversing
    changes made to 11e12f95e9ab80f27697e26fe92bd50568ad1643.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,10 @@
-import Header from "./components/header"
-import Hero from "./components/hero"
-import Features from "./components/features"
-import TechStack from "./components/tech-stack"
-import Reviews from "./components/reviews"
-import Faq from "./components/faq"
-import Contact from "./components/contact"
-import Footer from "./components/footer"
-import { ScrollProvider } from "./components/scroll-provider"
 
-export default function Home() {
+
+export default async function Home() {
+
   return (
-    <ScrollProvider>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main>
-          <Hero />
-          <Features />
-          <TechStack />
-          <Reviews />
-          <Faq />
-          <Contact />
-        </main>
-        <Footer />
-      </div>
-    </ScrollProvider>
-  )
+    <>
+     <div>Hello World</div>
+    </>
+  );
 }
-
